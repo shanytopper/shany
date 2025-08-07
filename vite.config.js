0f0d4b527a1f6fd94858e4ce086a864a3c5f7e67
shany topper <shanytopper@users.noreply.github.com>
2025-08-07 22:45:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/shany/', // your GitHub Pages repo name or the sub-path you’re serving from
   server: {
     host: true,
     port: 5173,
-    strictPort: true
-  }
-})
+    strictPort: true,
+  },
+});
